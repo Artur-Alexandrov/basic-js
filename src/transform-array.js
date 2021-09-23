8b48da4c8825ae11d69f277fe6d0811ffd43369f
Artur-Alexandrov <arturches@gmail.com>
2021-09-23 17:36:25 +0300

    add: add tranform solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  
+  if(!Array.isArray(arr)){
+    throw new Error ('\'arr\' parameter must be an instance of the Array!');
+  } 
+
+    let new_arr = [...arr] 
+    
+    function checker(el, i) {
+      if (el === '--double-prev' && i !== 0) {new_arr.splice(i, 1, new_arr[i - 1])}
+
+      else if (el === '--double-next'  && i !== new_arr.length - 1) {new_arr.splice(i, 1, new_arr[i + 1])}
+
+      else if (el === '--discard-next' && i !== new_arr.length - 1) {new_arr.splice(i, 2,)}
+
+      else if (el === '--discard-prev'  && i !== 0) {new_arr.splice(i-1, 2)}
+
+      else if ( el === '--double-prev'  && i === 0) {new_arr.splice(i, 1)}
+
+      else if ( el === '--discard-prev' && i === 0) {new_arr.splice(i, 1)}
+
+      else if (el === '--double-next' && i === new_arr.length - 1) {new_arr.splice(i, 1)}
+
+      else if (el === '--discard-next' && i === new_arr.length - 1) {new_arr.splice(i, 1)}
+          
+    }
+
+    new_arr.map(checker);
+      
+    
+  return new_arr
 }
